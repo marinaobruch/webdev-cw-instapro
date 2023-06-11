f9b01dd5a7259a1c9c16148de12bda0a42685bc6
«Marina» <marina_obruch@mail.ru>
2023-06-11 23:30:13 +0500

    Добавлена отрисовка добавления постов

--- components/add-post-page-component.js
+++ components/add-post-page-component.js
@@ -2,12 +2,25 @@ export function renderAddPostPageComponent({ appEl, onAddPostClick }) {
   const render = () => {
     // TODO: Реализовать страницу добавления поста
     const appHtml = `
-    <div class="page-container">
-      <div class="header-container"></div>
-      Cтраница добавления поста
-      <button class="button" id="add-button">Добавить</button>
-    </div>
-  `;
+    <div class="form">
+    <h3 class="form-title">Добавить пост</h3>
+      <div class="form-inputs">
+        <div class="uploan-image-container">
+          <div class="upload-image">
+          <label class="file-upload-label secondary-button">     
+          Выберите фото
+          </label>
+          </div>
+        </div>
+        <label>
+          Опишите фотографию:
+          <textarea class="input-text textarea" rows="4"></textarea>
+        </label>
+        <button class="add-button" id="add-button">Добавить</button>
+      </div>
+  </div>
+    `;
+
 
     appEl.innerHTML = appHtml;
 
